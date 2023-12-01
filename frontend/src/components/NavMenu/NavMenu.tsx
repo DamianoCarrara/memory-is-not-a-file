391ffc852633e908b4a0fc0232cb3f45e61a3153
DamianoCarrara <125041248+DamianoCarrara@users.noreply.github.com>
2023-12-01 19:39:35 +0100

    upp

--- frontend/src/components/NavMenu/NavMenu.tsx
+++ frontend/src/components/NavMenu/NavMenu.tsx
@@ -6,7 +6,6 @@ import { clsx } from 'clsx'
 import { useRouter } from 'next/router'
 import { theme } from '../../utils/theme'
 import { useStyles } from './NavMenu.styles'
-import { useStyles } from './NavMenu2.styles'
 import { locations, locationsBase } from '../../utils/locations'
 import { isFeatureEnabled } from '../../utils/features'
 
