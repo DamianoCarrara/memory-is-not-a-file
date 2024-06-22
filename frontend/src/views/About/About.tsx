802f1ba7d864e211402533d562cda49b7d42b704
DamianoCarrara <125041248+DamianoCarrara@users.noreply.github.com>
2024-06-22 08:40:02 +0200

    Update About.tsx
    video ext

--- frontend/src/views/About/About.tsx
+++ frontend/src/views/About/About.tsx
@@ -39,7 +39,7 @@ export const About: NextPage = () => {
 
           <p className={classes.video}>
             <video
-              controls preload poster="https://memoryisnotafile.com/_next/image/?url=%2F_next%2Fstatic%2Fmedia%2Fmain-image.6e6a7596.jpg&w=3840&q=75" >
+              controls preload poster="https://weblario.it/xxx/main-image.jpg" >
               <source src="https://weblario.it/xxx/minaf.mp4" />
               Your browser does not support the video tag...
             </video>
