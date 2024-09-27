adb359fe85d23e472177fa3ee871b0f291d6833e
DamianoCarrara <125041248+DamianoCarrara@users.noreply.github.com>
2024-09-27 19:39:56 +0200

    Add files via upload

---
+++ sanity-studio/schemas/news.ts
@@ -0,0 +1,50 @@
+export default {
+  name: "news",
+  title: "News",
+  type: "document",
+  fields: [
+    {
+      name: "medium",
+      title: "Titolo articolo",
+      type: "string",
+      description: "Titolo articolo",
+    },
+    {
+      name: "testata",
+      title: "Testata",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Descrizione",
+      type: "string",
+    },
+    {
+      name: "slug",
+      type: "slug",
+      title: "Url news",
+    },
+    {
+      name: "link",
+      type: "string",
+      title: "link"
+    },
+    {
+      name: "photo",
+      type: "image",
+      title: "Photo",
+      description: "Cover",
+    },
+    {
+      name: "pressGallery",
+      title: "Immagini",
+      description: "galleria immagini",
+      type: "array",
+      of: [
+        {
+          type: "image",
+        },
+      ],
+    },
+  ],
+};
