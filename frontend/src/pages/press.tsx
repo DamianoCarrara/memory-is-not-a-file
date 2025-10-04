2e1496363de712cfbd521565abe64f7f87d336a9
DamianoCarrara <125041248+DamianoCarrara@users.noreply.github.com>
2025-10-04 13:38:49 +0200

    Update press.tsx

--- frontend/src/pages/press.tsx
+++ frontend/src/pages/press.tsx
@@ -1,82 +1,85 @@
-declare const dimbox: any;
-import Link from "next/link";
-import groq from "groq";
-import imageUrlBuilder from "@sanity/image-url";
-import client from "../utils/sanityClient";
-import { PageTemplate } from "../components/PageTemplate/PageTemplate";
-import { PageTitle } from "../components/PageTitle/PageTitle";
-import styles from "../../ui/gal.module.css";
+  declare const dimbox: any;
+  import Link from "next/link";
+  import groq from "groq";
+  import imageUrlBuilder from "@sanity/image-url";
+  import client from "../utils/sanityClient";
+  import { PageTemplate } from "../components/PageTemplate/PageTemplate";
+  import { PageTitle } from "../components/PageTitle/PageTitle";
+  import styles from "../../ui/gal.module.css";
 
-function urlFor(source) {
-  return imageUrlBuilder(client).image(source);
-}
-const Press = ({ presses }) => { 
-  return (
-    <PageTemplate path="/press">
-      <PageTitle fullWidth title="Press" />
-      <ul className={styles.mainGalList}>
-        {presses.length > 0 &&
-          presses.map(
-            ({
-              _id,
-              medium = "",
-              slug = "",
-              photo = [],
-              description = "",
-              testata = "",
-              pressGallery = [],
-            }) =>
-             _id && (
-                <li key={_id} id={_id}>
-                  <div className={styles.mediumlinkimg}>
-                    <a
-                      href={urlFor(photo).url()}
-                      data-dimbox="my-gallery"
-                      data-dimbox-caption={medium}
-                      id={_id}
-                    >
-                      {photo && <img src={urlFor(photo).url()} />}
-                    </a>
-                  </div>
-                  <div className={styles.mediumlink}>
-                    <h2>{testata}</h2>
-                    <h3>{medium}</h3>
-                    <p>{description}</p>
+  function urlFor(source) {
+    return imageUrlBuilder(client).image(source);
+  }
+  const Press = ({ presses }) => {
+    return (
+      <PageTemplate path="/press">
+        <PageTitle fullWidth title="Press" />
+        <ul className={styles.mainGalList}>
+          {presses.length > 0 &&
+            presses.map(
+              ({
+                _id,
+                medium = "",
+                slug = "",
+                photo = [],
+                description = "",
+                testata = "",
+                link = "",
+                pressGallery = [],
+              }) =>
+                _id && (
 
-                    <ul className={styles.gal}>
-                      {pressGallery.map((presgal) => (
-                        <li key={presgal}>
-                          {presgal && (
-                            <a
-                              href={urlFor(presgal).url()}
-                              data-dimbox="my-gallery"
-                              data-dimbox-caption={medium}
-                              id={_id}
-                            >
-                              <img src={urlFor(presgal).url()} />
-                            </a>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              )
-          )}
-      </ul>
-    </PageTemplate>
-  );
-};
+                  <li key={_id}>
+                    <div className={styles.mediumlinkimg}>
+                      <a
+                        href={urlFor(photo).url()}
+                        data-dimbox="my-gallery"
+                        data-dimbox-caption={medium}
+                        id={_id}
+                      >
+                        {photo && <img src={urlFor(photo).url()} />}
+                      </a>
+                    </div>
+                    <div className={styles.mediumlink}>
+                      <h2>{testata}</h2>
+                      <h3>{medium}</h3>
+                      <p><a href={link && link} alt={testata} target="_blank">Link</a></p>
+                      <p>{description}</p>
 
-export async function getStaticProps() {
-  const presses = await client.fetch(groq`
-    *[_type == "press"] | order(_createdAt desc)
-  `);
-  return {
-    props: {
-      presses,
-    },
+                      <ul className={styles.gal}>
+                        {pressGallery.map((presgal) => (
+                          <li key={presgal}>
+                            {presgal && (
+                              <a
+                                href={urlFor(presgal).url()}
+                                data-dimbox="my-gallery"
+                                data-dimbox-caption={medium}
+                                id={_id}
+                              >
+                                <img src={urlFor(presgal).url()} />
+                              </a>
+                            )}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </li>
+                )
+            )}
+        </ul>
+      </PageTemplate>
+    );
   };
-}
 
-export default Press;
+  export async function getStaticProps() {
+    const presses = await client.fetch(groq`
+      *[_type == "press"] | order(_createdAt desc)
+    `);
+    return {
+      props: {
+        presses,
+      },
+    };
+  }
+
+  export default Press;
