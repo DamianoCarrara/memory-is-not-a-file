f35ca12c66729977a66c8a71aecda0cba4057d7a
DamianoCarrara <125041248+DamianoCarrara@users.noreply.github.com>
2025-10-17 00:21:26 +0200

    Update about.tsx

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,7 +1,6 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { About } from '../views/About/About'
 import { GetStaticProps } from '../types'
-
 export default About
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
