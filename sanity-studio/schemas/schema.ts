15337a5c957a7576441a6aec641de2bba32034a7
DamianoCarrara <125041248+DamianoCarrara@users.noreply.github.com>
2026-02-12 22:56:00 +0100

    Update schema.ts

--- sanity-studio/schemas/schema.ts
+++ sanity-studio/schemas/schema.ts
@@ -5,7 +5,6 @@ import news from "./news";
 import category from "./category";
 import sellingOption from "./sellingOption";
 import page from "./page";
-
 export default [
   localeString,
   photo,
